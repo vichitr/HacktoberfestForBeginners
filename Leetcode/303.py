05d9c024f477a5c08cbe9ac3bb6db8153c6e7b57
Luis Herrera <luhegoes@gmail.com>
2019-10-12 02:23:01 -0500

    Add leetcode problems 303 and 676

---
+++ Leetcode/303.py
@@ -0,0 +1,29 @@
+'''
+Problem: 303. Range Sum Query - Immutable
+https://leetcode.com/problems/range-sum-query-immutable/
+Author: Luis Herrera
+Summary: We use a prefix array to make the queries in constant time O(1).
+'''
+
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.prefix_sum = [0] * len(nums)
+        if len(nums) > 0:
+            self.prefix_sum[0] = nums[0]
+            for i in range(1, len(nums)):
+                self.prefix_sum[i] = self.prefix_sum[i - 1] + nums[i]
+        
+
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        if i > 0 :
+            return self.prefix_sum[j] - self.prefix_sum[i - 1]
+        return self.prefix_sum[j]
