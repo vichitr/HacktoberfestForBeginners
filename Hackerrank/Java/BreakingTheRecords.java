8d524391e2885a6975538da6290e7b2ede7776a7
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2019-10-12 20:34:50 +0530

    Solution to Breaking the records in Java

---
+++ Hackerrank/Java/BreakingTheRecords.java
@@ -0,0 +1,69 @@
+/*
+MY SOLUTION FOR BREAKING THE RECORD PROBLEM
+LINK TO THE PROBLEM : https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the breakingRecords function below.
+    static int[] breakingRecords(int[] scores) {
+        int i, highest=scores[0], lowest=scores[0], rec[]=new int[2];
+        for(i=1; i<scores.length; i++)
+        {
+            if(scores[i]>highest)
+            {
+                highest = scores[i];
+                rec[0]++;
+            }
+            if(scores[i]<lowest)
+            {
+                lowest = scores[i];
+                rec[1]++;
+            }
+        }
+        return rec;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] scores = new int[n];
+
+        String[] scoresItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int scoresItem = Integer.parseInt(scoresItems[i]);
+            scores[i] = scoresItem;
+        }
+
+        int[] result = breakingRecords(scores);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
