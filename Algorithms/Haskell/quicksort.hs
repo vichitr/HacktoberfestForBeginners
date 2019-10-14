b434f8bf80d67ca7595f136732922e50689bbcbe
namanbiyani <namanb@iitk.ac.in>
2019-10-14 15:37:08 +0530

    Added quicksort in haskell

---
+++ Algorithms/Haskell/quicksort.hs
@@ -0,0 +1,7 @@
+-- Quicksort program in haskell
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted  
