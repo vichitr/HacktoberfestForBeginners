44cbf594c893c3b1873446caeea63a15eb2e5776
Jerry Shueh <shueh.jerry@gmail.com>
2019-10-18 16:40:25 -0400

    basic threadedtree class

--- DS/C++/threadedtree.h
+++ DS/C++/threadedtree.h
@@ -9,3 +9,26 @@ This implementation supports generic typing and both forward and backward in-ord
 Iterator implementation also included to facilitate easy traversal via operators
 
 ***************************************************/
+
+#include <iostream>
+using namespace std;
+
+template <class T>
+class ThreadedTree {
+	struct Node{
+		T data_;
+		Node* left_;
+		Node* right_;
+		int lFlag_;		// Whether the node has a left thread, 0 = no, 1 = yes
+		int rFlag_;		// Whether the node has a right thread, 0 = no, 1 = yes
+
+		Node(const T& data, Node* lt = nullptr, Node* rt = nullptr, int lFlag = 0, int rFlag = 0) {
+			data_ = data;
+			left_ = lt;
+			right_ = rt;
+			lFlag_ = lFlag;
+			rFlag_ = rFlag;
+		}
+	};
+	Node* root_;
+};
