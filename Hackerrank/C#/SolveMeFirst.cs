dd21a852bbe571a6711a7d9b2f29793fb2c32ca1
Jared Bardell <54115604+jbardell19@users.noreply.github.com>
2019-10-19 17:51:09 -0500

    Added line
    return a+b;

--- Hackerrank/C#/SolveMeFirst.cs
+++ Hackerrank/C#/SolveMeFirst.cs
@@ -4,8 +4,8 @@ using System.IO;
 class Solution {
 
     static int solveMeFirst(int a, int b) { 
-      // Hint: Type return a+b; below  
-      
+    return a+b;
+        
     }
 
     static void Main(String[] args) {
