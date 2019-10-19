24a876fb673ad6e32c8531504aa8dfae3c705cfa
Jared Bardell <54115604+jbardell19@users.noreply.github.com>
2019-10-19 17:45:27 -0500

    Create SolveMeFirst.cs
    C# Folder

---
+++ Hackerrank/C#/SolveMeFirst.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+
+    static int solveMeFirst(int a, int b) { 
+      // Hint: Type return a+b; below  
+      
+    }
+
+    static void Main(String[] args) {
+        int val1 = Convert.ToInt32(Console.ReadLine());
+        int val2 = Convert.ToInt32(Console.ReadLine());
+        int sum = solveMeFirst(val1,val2);
+        Console.WriteLine(sum);
+    }
+}      
