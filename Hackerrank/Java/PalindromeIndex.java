23e1a601e84db13405f8e64b0dc9349fbad76ebf
Janitha133 <33251135+Janitha133@users.noreply.github.com>
2019-10-20 10:34:01 +0530

    update Palindrome Index

--- Hackerrank/Java/PalindromeIndex.java
+++ Hackerrank/Java/PalindromeIndex.java
@@ -8,31 +8,37 @@ import java.util.regex.*;
 
 public class Solution {
 
-    // Complete the palindromeIndex function below.
-    static int palindromeIndex(String s) {
-
-
-    }
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int q = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int qItr = 0; qItr < q; qItr++) {
-            String s = scanner.nextLine();
-
-            int result = palindromeIndex(s);
-
-            bufferedWriter.write(String.valueOf(result));
-            bufferedWriter.newLine();
+public static boolean palindrome(String s){
+    StringBuffer sb = new StringBuffer();
+        sb = sb.append(s);
+        sb = sb.reverse();
+        String n = sb.toString();
+       if(n.equals(s)){
+           return true;
+       }
+    else return false;
+}
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for(int a=0; a<n; a++){
+        int out = 0;
+        String input = sc.next();           
+        if(palindrome(input)){
+            System.out.println("-1");
         }
-
-        bufferedWriter.close();
-
-        scanner.close();
+        else{
+            for(int i=0; i<input.length(); i++){
+                StringBuilder ss = new StringBuilder(input);
+                if(input.charAt(i)!=input.charAt(input.length()-i-1)){
+                 if(palindrome(ss.deleteCharAt(i).toString())){
+                     System.out.println(i);
+                 }
+                    else System.out.println(input.length()-i-1);
+                         break;
+                }
+            }
+        }
     }
+}
 }
