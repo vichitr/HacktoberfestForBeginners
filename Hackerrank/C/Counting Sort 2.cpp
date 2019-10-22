24d5e768ca09fe4453829d167657a4aaeacd3ad7
Rajarshi24 <40721635+Rajarshi24@users.noreply.github.com>
2019-10-23 01:10:19 +0530

    Add files via upload

---
+++ Hackerrank/C/Counting Sort 2.cpp
@@ -0,0 +1,147 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+// Complete the countingSort function below.
+
+// Please store the size of the integer array to be returned in result_count pointer. For example,
+// int a[3] = {1, 2, 3};
+//
+// *result_count = 3;
+//
+// return a;
+//
+int* countingSort(int arr_count, int* arr, int* result_count) {
+    int flag,k=0;
+    int temp[100];
+    * result_count=arr_count;
+    int *result=malloc(arr_count*sizeof(int));
+    for(int i=0;i<100;i++)
+        temp[i]=0;
+    for(int i=0;i<arr_count;i++)
+        {
+            flag=arr[i];
+            temp[flag]++;
+        }
+    for(int i=0;i<100;i++)
+        {   
+            if(temp[i]==0)
+                continue;
+            else
+                {
+                    for(int j=0;j<temp[i];j++)
+                        {
+                            result[k]=i;
+                            k++;
+                        }
+                }
+        }
+    return result;
+
+
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* n_endptr;
+    char* n_str = readline();
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char** arr_temp = split_string(readline());
+
+    int* arr = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        char* arr_item_endptr;
+        char* arr_item_str = *(arr_temp + i);
+        int arr_item = strtol(arr_item_str, &arr_item_endptr, 10);
+
+        if (arr_item_endptr == arr_item_str || *arr_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        *(arr + i) = arr_item;
+    }
+
+    int arr_count = n;
+
+    int result_count;
+    int* result = countingSort(arr_count, arr, &result_count);
+
+    for (int i = 0; i < result_count; i++) {
+        fprintf(fptr, "%d", *(result + i));
+
+        if (i != result_count - 1) {
+            fprintf(fptr, " ");
+        }
+    }
+
+    fprintf(fptr, "\n");
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
