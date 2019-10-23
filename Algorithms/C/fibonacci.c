df4cfa5ac4ae72f6b4285027218754e6ad4f81f9
Viktor Guilherme <viktorguilherme@gmail.com>
2019-10-23 19:02:57 -0300

    fibonacci.c

--- Algorithms/C/fibonacci.c
+++ Algorithms/C/fibonacci.c
@@ -1,4 +1,30 @@
 #include <stdio.h>
+#include <omp.h> //openmp library
+
+long fib(long n) 
+{
+  long i, j;
+
+  if (n < 2)
+    return n;
+  else if (n < z) 
+  {
+    return fib(n-1) + fib (n-2);
+  }
+  else 
+  {
+    #pragma omp parallel sections
+    {
+      #pragma omp section
+      i = fib(n-1);
+      #pragma omp section
+      j = fib(n-2);
+    }
+    return i + j;
+  }
+}
+
+
 
 int main() {
     int i, n, t = 0, t1 = 1, nextTerm;
