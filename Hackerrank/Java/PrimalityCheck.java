5a8cbcf1e882dd136702d8d18ea3aa3d4c2b2017
arsalan7866 <42116394+arsalan7866@users.noreply.github.com>
2020-09-30 22:11:08 +0530

    Add files via upload

---
+++ Hackerrank/Java/PrimalityCheck.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+        BigInteger a =new BigInteger(n);
+boolean result = a.isProbablePrime(1);
+if(result){
+System.out.println("prime");}
+else System.out.println("not prime");
+        scanner.close();
+    }
+}
