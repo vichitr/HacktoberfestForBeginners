7cc831bf5f3a72cef935d4e31eeb9546bc03b966
NayanPrakash11 <69430088+NayanPrakash11@users.noreply.github.com>
2020-10-01 00:23:57 +0530

    Update hellohack.py

--- HelloWorld/hellohack.py
+++ HelloWorld/hellohack.py
@@ -1,1 +1,10 @@
+// methods to implement Hello hactoberfest by different ways
 print("Hello hacktoberfest")
+
+print('Hello hactoberfest')
+
+print("Hello" + "hactoberfest")
+
+a = "Hello"
+b = "hactoberfest"
+print(a +' '+ b)
