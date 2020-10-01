8bb44cbb61fbccffc2afac11a92aee217751eb6a
aj-spec <ayushi.jain2019@vitstudent.ac.in>
2020-10-01 21:53:14 +0530

    Add Ayushi Jain to Contributors list

--- HelloWorld/hello.cpp
+++ HelloWorld/hello.cpp
@@ -5,5 +5,13 @@ using namespace std;
 int main() {
     cout << "Hello World!" ;
     cout<< endl;
+    int a,r,n;
+    n=687;
+    while(n!=0)
+    {
+        a=n%10;
+        r=r+a*10;
+        n=n/10;
+    }
     return 0;
 }
