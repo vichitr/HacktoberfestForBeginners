daa6468781d39be0a70e225048cef806453b5d95
Sairam <48212872+sairam008@users.noreply.github.com>
2020-10-02 03:06:22 +0530

    Add files via upload

--- LeetCode/11_Container_with_most_water.py
+++ LeetCode/11_Container_with_most_water.py
@@ -1,14 +1,16 @@
-class Solution:
-    def maxArea(height):
-        maxa=0
-        lptr=0
-        rptr=len(height)-1
-        
-        while(lptr<rptr):
-            area=min(height[lptr],height[rptr])*(rptr-lptr)
-            maxa=max(maxa,area)
-            if height[lptr] < height[rptr]:
-                lptr=lptr+1
-            else:
-                rptr=rptr-1
-        return maxa
+def maxArea(height):
+    maxa=0
+    lptr=0
+    rptr=len(height)-1
+    
+    while(lptr<rptr):
+        area=min(height[lptr],height[rptr])*(rptr-lptr)
+        maxa=max(maxa,area)
+        if height[lptr] < height[rptr]:
+            lptr=lptr+1
+        else:
+            rptr=rptr-1
+    return maxa
+
+inp=list(map(int, input().split(" ")))
+print(maxArea(inp))
