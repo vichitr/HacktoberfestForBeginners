c30affc04b8413457d85e970425789efe10fa81c
ATUL KUMAR RAJAK <50494426+atul070@users.noreply.github.com>
2020-10-01 13:43:13 +0530

    Say Hello World added

---
+++ Hackerrank/C++/001. Say Hello, World! With Cpp.cpp
@@ -0,0 +1,10 @@
+// Problem: https://www.hackerrank.com/challenges/cpp-hello-world/problem
+// Score: 10
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!";
+    return 0;
+}
