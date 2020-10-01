8f63f09bc54cb745e064ff3ce81ac52f64f8c798
AnuragGupta806 <54318429+AnuragGupta806@users.noreply.github.com>
2020-10-01 10:11:16 +0530

    Added problem 1328B solution

---
+++ Codeforces/1328B.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, k;
+        cin >> n >> k;
+        string s(n, 'a');
+        for (int i = n - 2; i >= 0; i--) {
+            if (k <= (n - i - 1)) {
+                s[i] = 'b';
+                s[n - k] = 'b';
+                cout << s << endl;
+                break;
+            }
+            k -= (n - i - 1);
+        }
+    }
+}
