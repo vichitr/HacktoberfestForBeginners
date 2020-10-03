c286e0235a2463ee9fb13eed2bc9bed52d7e33f3
rohanap <36108401+rohanap@users.noreply.github.com>
2020-10-03 11:07:20 +0530

    Update LikeOprator.sql
    Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.

--- Hackerrank/sql/LikeOprator.sql
+++ Hackerrank/sql/LikeOprator.sql
@@ -1,3 +1,7 @@
+/*
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+*/
+
 SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY NOT LIKE '[a,e,i,o,u]%'
