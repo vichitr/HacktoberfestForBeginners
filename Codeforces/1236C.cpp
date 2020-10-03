8d8f7b52d6b48eb965a9cb1606a06933ea7e4d16
joaldotavares <joaldo.junior@ucsal.edu.br>
2020-10-03 18:25:02 -0300

    question 1236c resolved

--- Codeforces/1236C.cpp
+++ Codeforces/1236C.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-using LL =long long;
-LL x,y,z;
+typedef long long LL;
 
 int main() {
     LL n,k=1,m=0;
     cin>>n;
-    int a[n][n]={0};
+    int a[n][n];
         for(int i=0;i<n;i++){
             m=m^1;
             for(int j=0;j<n;j++)
