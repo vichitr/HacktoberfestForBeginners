e25250d0c0e4188016896d1c52a5d0adbf8c06f1
Rupali409 <54534133+Rupali409@users.noreply.github.com>
2020-10-11 21:47:33 +0530

    Updated python if else

--- Hackerrank/Python/Python If-Else.py
+++ Hackerrank/Python/Python If-Else.py
@@ -11,3 +11,25 @@ if __name__ == '__main__':
         print("Not Weird")
     elif n % 2 == 1:
         print("Weird")
+        
+        
+ # created by Rupali
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    if n%2==0 :
+        if n%2==0 & n>20 |  2<=n<=5 :
+           print("Not Weird")    
+
+        elif 6<=n<=20  :
+         print("Weird")
+
+        else:
+            print("Not Weird") 
+
+       
+
+   
+    else :
+      print("Weird")  
