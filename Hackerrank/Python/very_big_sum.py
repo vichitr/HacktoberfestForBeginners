41a17c27a1e4bc8055683f3a1101522e1ae0297b
Abel Koshy Mathew <55405281+abelkmathew@users.noreply.github.com>
2020-10-17 18:45:27 +0530

    Update very_big_sum.py

--- Hackerrank/Python/very_big_sum.py
+++ Hackerrank/Python/very_big_sum.py
@@ -1,9 +1,3 @@
-import math
-import os
-import random
-import re
-import sys
-
 # Complete the aVeryBigSum function below.
 def aVeryBigSum(n,ar):
     sum=0
@@ -12,15 +6,4 @@ def aVeryBigSum(n,ar):
     return sum    
 
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    ar_count = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = aVeryBigSum(ar_count,ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
