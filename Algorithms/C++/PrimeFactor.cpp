b964ca1d7e7ee01d0b0e5cc464b25f27270cd789
KishkinJ10 <81019895+KishkinJ10@users.noreply.github.com>
2021-03-22 20:03:52 +0530

    Create PrimeFactor.cpp
    i am adding prime factorization using sieve of eratothene

---
+++ Algorithms/C++/PrimeFactor.cpp
@@ -0,0 +1,32 @@
+// Prime factorization using sieve of Eratosthenes
+#include<iostream>
+using namespace std;
+
+void primeFactor(int n){
+    int spf[100]={0};
+    for(int i=2;i<=n;i++){
+        spf[i]=i;
+
+    }
+    for(int i=2;i<=n;i++){
+        if(spf[i]==i){
+            for(int j=i*i;j<=n;j+=i){
+                if(spf[j]==j){
+                    spf[j]=i;
+                }
+            }
+        }
+    }
+    while(n!=1){
+        cout<<spf[n]<<" ";
+        n=n/spf[n];
+    }
+}
+
+
+int main(){
+int n;
+cin>>n;
+primeFactor(n);
+    return 0;
+}
