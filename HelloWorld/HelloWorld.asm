d9527b1e54d538dd68a60d605b7555d4877ce47b
Vicky <107568580+HungryVicky@users.noreply.github.com>
2022-10-08 18:35:19 +0530

    Create HelloWorld.asm

---
+++ HelloWorld/HelloWorld.asm
@@ -0,0 +1,19 @@
+section .data
+	msg db "Hello World",0xa
+	len equ $- msg
+
+section .text
+	global _start
+	
+_start:
+	mov eax ,4
+	mov ebx ,1
+	mov ecx ,msg
+	mov edx ,len
+
+	int 0x80
+
+	mov eax,1
+	int 0x80
+  
+  //nasm 386
