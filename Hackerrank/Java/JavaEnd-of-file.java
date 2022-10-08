db3c39c2ae1f891a8379000d9042436b12670355
Vicky <107568580+HungryVicky@users.noreply.github.com>
2022-10-08 18:32:50 +0530

    Create JavaEnd-of-file.java

---
+++ Hackerrank/Java/JavaEnd-of-file.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       int i = 0;
+       while(scanner.hasNext()){
+           i++;
+           System.out.println(i+" "+scanner.nextLine());
+           
+       }
+       
+       
+       
+    }
+}
