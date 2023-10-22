86dc2e9f50f566037f7194e6ee3a2103521726fa
Adarsh Kumar <117112030+adarsh-k27r@users.noreply.github.com>
2023-10-22 20:03:48 +0530

    added 13_RomanToInteger.cpp

---
+++ LeetCode/13_RomanToInteger.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans=0;
+        unordered_map <char,int> mp{
+        {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+
+    for(int i=0;i<s.size();i++){
+        if(mp[s[i]]<mp[s[i+1]]){
+            //for cases such as IV,CM, XL, etc...
+            ans=ans-mp[s[i]];
+        }
+        else{
+            ans=ans+mp[s[i]];
+        }
+    }
+        return ans;
+    }
+};
